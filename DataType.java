9cf9e61b12ef13203c26b1fe3584f2d66eeda0bc
diksha172005 <dikshatiwari1723@gmail.com>
2026-03-29 22:12:19 +0530

    Initial commit

---
+++ DataType.java
@@ -0,0 +1,27 @@
+public class DataType{
+    
+    public static void main(String[] args) {
+        byte b = 10;   // stores 1 byte.
+        short s = 30;  //2 byte.
+        int i = 17;    // 4 bytes.
+        long l = 1000L;   // 8 bytes.
+        float f = 3.17f;  // 4 bytes
+        double d = 3.122; // 8 bytes.
+        char c = 'A';    //2 bytes.
+        boolean bln = true; //1 byte.
+        String str = "diksha"; 
+        int[] arr = {1,2,3,4,5,6};
+
+        System.out.println("byte:" +b);
+        System.out.println("short:" +s);
+        System.out.println("integer:" +i);
+        System.out.println("long:" +l);
+        System.out.println("Float:" +f);
+        System.out.println("Double:" +d);
+        System.out.println("character:" +c);
+        System.out.println("Boolean:" +bln);
+        System.out.println("String:" +str);
+        System.out.println("Array:" +arr);
+
+    }
+}
