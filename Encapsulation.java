c90a9e941daec6057bef4e2d31edc959986fa3c1
diksha172005 <dikshatiwari1723@gmail.com>
2026-04-08 22:22:49 +0530

    Day_11

---
+++ Encapsulation.java
@@ -0,0 +1,39 @@
+public class Encapsulation {
+    private String name;
+    private int age;
+    private double salary;
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+
+    // Getter for salary
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setter for salary
+    public void setSalary(double salary) {
+        if (salary > 0) {
+            this.salary = salary;
+        }
+    }
+}
